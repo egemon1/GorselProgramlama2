cae01b1e6deab8b950a04836d8e5927871c86236
egemon1 <ege.ersoz@hotmail.com>
2025-10-02 11:53:27 +0300

    Add files via upload

---
+++ gorsel2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp12
+{
+    public partial class Form1 : Form
+    {
+        private bool konum = false;
+        private int mouseX, mouseY;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseDown(object sender, MouseEventArgs e)
+        {
+            konum = true;
+            mouseX = e.X;
+            mouseY = e.Y;
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(konum)
+            {
+                button1.Left += e.X - mouseX;
+                button1.Top += e.Y - mouseY;
+            }
+        }
+
+        private void button1_MouseUp(object sender, MouseEventArgs e)
+        {
+            konum = false;
+        }
+    }
+}
