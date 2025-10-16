f8b7776e72d125b00e891a279a114a8a90c29cb1
egemon1 <ege.ersoz@hotmail.com>
2025-10-16 12:00:36 +0300

    Add files via upload

---
+++ Sayı Oyunu/Sayı Oyunu/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Sayı_Oyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnBasla = new System.Windows.Forms.Button();
+            this.btnBitir = new System.Windows.Forms.Button();
+            this.gbOyunAlani = new System.Windows.Forms.GroupBox();
+            this.lbTiklananSayilar = new System.Windows.Forms.ListBox();
+            this.lblSure = new System.Windows.Forms.Label();
+            this.oyunZamanlayici = new System.Windows.Forms.Timer(this.components);
+            this.butonYerDegistirici = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnBasla
+            // 
+            this.btnBasla.Location = new System.Drawing.Point(45, 46);
+            this.btnBasla.Name = "btnBasla";
+            this.btnBasla.Size = new System.Drawing.Size(139, 143);
+            this.btnBasla.TabIndex = 0;
+            this.btnBasla.Text = "Oyuna Başla";
+            this.btnBasla.UseVisualStyleBackColor = true;
+            this.btnBasla.Click += new System.EventHandler(this.btnBasla_Click_1);
+            // 
+            // btnBitir
+            // 
+            this.btnBitir.Location = new System.Drawing.Point(1144, 46);
+            this.btnBitir.Name = "btnBitir";
+            this.btnBitir.Size = new System.Drawing.Size(157, 115);
+            this.btnBitir.TabIndex = 1;
+            this.btnBitir.Text = "Oyunu Bitir";
+            this.btnBitir.UseVisualStyleBackColor = true;
+            this.btnBitir.Click += new System.EventHandler(this.btnBitir_Click);
+            // 
+            // gbOyunAlani
+            // 
+            this.gbOyunAlani.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.gbOyunAlani.Location = new System.Drawing.Point(285, 14);
+            this.gbOyunAlani.Name = "gbOyunAlani";
+            this.gbOyunAlani.Size = new System.Drawing.Size(650, 630);
+            this.gbOyunAlani.TabIndex = 2;
+            this.gbOyunAlani.TabStop = false;
+            this.gbOyunAlani.Text = "Oyun Alanı";
+            // 
+            // lbTiklananSayilar
+            // 
+            this.lbTiklananSayilar.FormattingEnabled = true;
+            this.lbTiklananSayilar.ItemHeight = 16;
+            this.lbTiklananSayilar.Location = new System.Drawing.Point(957, 14);
+            this.lbTiklananSayilar.Name = "lbTiklananSayilar";
+            this.lbTiklananSayilar.Size = new System.Drawing.Size(162, 628);
+            this.lbTiklananSayilar.TabIndex = 4;
+            this.lbTiklananSayilar.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // lblSure
+            // 
+            this.lblSure.AutoEllipsis = true;
+            this.lblSure.Location = new System.Drawing.Point(1177, 313);
+            this.lblSure.Name = "lblSure";
+            this.lblSure.Size = new System.Drawing.Size(100, 99);
+            this.lblSure.TabIndex = 5;
+            this.lblSure.Text = "Süre";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1378, 654);
+            this.Controls.Add(this.lblSure);
+            this.Controls.Add(this.lbTiklananSayilar);
+            this.Controls.Add(this.gbOyunAlani);
+            this.Controls.Add(this.btnBitir);
+            this.Controls.Add(this.btnBasla);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBasla;
+        private System.Windows.Forms.Button btnBitir;
+        private System.Windows.Forms.GroupBox gbOyunAlani;
+        private System.Windows.Forms.ListBox lbTiklananSayilar;
+        private System.Windows.Forms.Label lblSure;
+        private System.Windows.Forms.Timer oyunZamanlayici;
+        private System.Windows.Forms.Timer butonYerDegistirici;
+    }
+}
+
---
+++ Sayı Oyunu/Sayı Oyunu/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sayı_Oyunu
+{
+    public partial class Form1 : Form
+    {
+        Random rastgele = new Random();
+        List<int> oyunSayilari = new List<int>();
+        List<int> tiklananNumaralar = new List<int>();
+        int kalanSure = 60;
+        bool oyunBasladi = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            OyunuHazirla();
+        }
+
+        private void OyunuHazirla()
+        {
+            oyunSayilari.Clear();
+            tiklananNumaralar.Clear();
+            gbOyunAlani.Controls.Clear();
+
+            btnBitir.Visible = false;
+            btnBasla.Visible = true;
+            lblSure.Text = $"{kalanSure} sn";
+
+            for (int i = 0; i < 10; i++)
+            {
+                oyunSayilari.Add(rastgele.Next(1, 101));
+            }
+
+            int x = 10, y = 10;
+            foreach (var num in oyunSayilari)
+            {
+                Button btn = new Button();
+                btn.Text = num.ToString();
+                btn.Size = new System.Drawing.Size(50, 50);
+                btn.Location = new System.Drawing.Point(x, y);
+                btn.Click += Button_Click;
+                gbOyunAlani.Controls.Add(btn);
+
+                x += 60;
+                if (x > 300)
+                {
+                    x = 10;
+                    y += 60;
+                }
+            }
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            if (!oyunBasladi) return;
+
+            Button tiklananButon = sender as Button;
+            int numara = int.Parse(tiklananButon.Text);
+
+            tiklananButon.Visible = false;
+            tiklananNumaralar.Add(numara);
+
+            lbTiklananSayilar.Items.Add(numara);
+
+            if (tiklananNumaralar.Count == 10)
+            {
+                OyunDurumunuKontrolEt();
+            }
+        }
+
+        private void OyunDurumunuKontrolEt()
+        {
+            List<int> dogruSiralama = oyunSayilari.Where(n => n % 2 == 0).OrderBy(n => n).ToList();
+            List<int> kullaniciSiralama = tiklananNumaralar.ToList();
+
+            if (dogruSiralama.SequenceEqual(kullaniciSiralama))
+            {
+                MessageBox.Show("Kazandınız!");
+            }
+            else
+            {
+                string dogruSiralamaStr = string.Join(", ", dogruSiralama);
+                string kullaniciSiralamaStr = string.Join(", ", kullaniciSiralama);
+
+                MessageBox.Show($"Kaybettiniz! Sayılar yanlış sırada veya tek sayı var.\n\n" +
+                                 $"Doğru Sıralama: {dogruSiralamaStr}\n" +
+                                 $"Sizin Sıralamanız: {kullaniciSiralamaStr}");
+            }
+        }
+
+        private void OyunZamanlayici_Tick(object sender, EventArgs e)
+        {
+            kalanSure--;
+            lblSure.Text = $"{kalanSure} sn";
+
+            if (kalanSure == 0)
+            {
+                oyunZamanlayici.Stop();
+                MessageBox.Show("Zaman Bitti! Kaybettiniz.");
+            }
+        }
+
+        private void ButonYerDegistirici_Tick(object sender, EventArgs e)
+        {
+            if (!oyunBasladi) return;
+
+            foreach (Button buton in gbOyunAlani.Controls)
+            {
+                int x = rastgele.Next(gbOyunAlani.Width - buton.Width);
+                int y = rastgele.Next(gbOyunAlani.Height - buton.Height);
+                buton.Location = new System.Drawing.Point(x, y);
+            }
+        }
+
+        private void btnBasla_Click(object sender, EventArgs e)
+        {
+            oyunBasladi = true;
+            btnBasla.Visible = false;
+            btnBitir.Visible = true;
+            oyunZamanlayici.Start();
+            kalanSure = 60;
+            lblSure.Text = $"{kalanSure} sn";
+
+            butonYerDegistirici.Start();
+        }
+
+        private void btnBitir_Click(object sender, EventArgs e)
+        {
+            oyunZamanlayici.Stop();
+            butonYerDegistirici.Stop();
+            foreach (Button item in gbOyunAlani.Controls)
+            {
+                if (item is Button)
+                {
+                    item.Enabled = false;
+                }
+            }
+            OyunDurumunuKontrolEt();
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBasla_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
